a333f933aa5436fdb4e3cc7fc20ca23df10dbd57
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-03-19 12:29:27 -0400

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,15 +1,21 @@
+if(!state.notesInitialize){
+  state.notesRecord = []
+  state.notesEndBy = 0
+  state.notesInitialize = true
+}
+
+//this function finds, parses, and extracts Note++ notes
 function noteTextParser (){
   tempText = text
   state.notes = []
 
   //this finds and caches Note++ formatted text
-  authorsNotes = [...tempText.matchAll(/\{(a\/?n(\d+)):([^\{\}]*)\}\n?/gi)]
-  unformattedNotes = [...tempText.matchAll(/\{(\d+):([^\{\}]*)\}\n?/gi)]
-  editorsNotes = [...tempText.matchAll(/\{(e\/?n(\d+)):([^\{\}]*)\}\n?/gi)]
-  editorsNotesScene = [...tempText.matchAll(/\{(e\/?ns(\d+)):([^\{\}]*)\}\n?/gi)]
+  authorsNotes = [...tempText.matchAll(/\{(\d*)(a\/?n(\d+)):([^\{\}]*)\}\n?/gi)]
+  unformattedNotes = [...tempText.matchAll(/\{(\d*)([\s-,\.]?(\d+)):([^\{\}]*)\}\n?/gi)]
+  editorsNotes = [...tempText.matchAll(/\{(\d*)(e\/?n(\d+)):([^\{\}]*)\}\n?/gi)]
+  editorsNotesScene = [...tempText.matchAll(/\{(\d*)(e\/?ns(\d+)):([^\{\}]*)\}\n?/gi)]
 
   //this removes Note++ formatted text from the context
-  //and tracks how m
   for (i = 0; i < authorsNotes.length; i++){
     tempText = tempText.replace(authorsNotes[i][0], "")
   }
@@ -26,56 +32,100 @@ function noteTextParser (){
   //this reformats the cached Note++ text into an array of 
   //objects ready to be injected into the context later
   for(i = 0; i < authorsNotes.length; i++){
-    if(/\{a\/?n\d+:\s+\}\n?/i.test(authorsNotes[i][0])){
-      authorsNotes[i][3] = ""
+    for(ii = 0; ii < state.notesRecord.length; ii++){
+      if(state.notesRecord[ii].record == authorsNotes[i][0]){
+        state.notesRecordMatch = true
+      }
     }
-    tempNote = authorsNotes[i][0].replace(`{${authorsNotes[i][1]}`, "[Author's note")
+    if(/\{\d*a\/?n\d+:\s+\}\n?/i.test(authorsNotes[i][0])){
+      authorsNotes[i][4] = ""
+    }
+    tempNote = authorsNotes[i][0].replace(`{${authorsNotes[i][1]}${authorsNotes[i][2]}`, "[Author's note")
     tempNote = tempNote.replace("}\n", "}")
     tempNote = tempNote.replace("}", "]")
-    tempLine = Number(authorsNotes[i][2])
-    tempObject = {note: tempNote, line: tempLine}
-    if(authorsNotes[i][3]){
+    tempLine = Number(authorsNotes[i][3])
+    tempEndBy = Number(authorsNotes[i][1]) ? info.actionCount + Number(authorsNotes[i][1]) : 0
+    tempRecord = {record: authorsNotes[i][0], endBy: tempEndBy}
+    tempObject = {record: authorsNotes[i][0], note: tempNote, line: tempLine}
+    if(authorsNotes[i][4]){
       state.notes.push(tempObject)
     }
+    if(!state.notesRecordMatch){
+      state.notesRecord.push(tempRecord)
+    }
+    state.notesRecordMatch = false
   }
   for(i = 0; i < unformattedNotes.length; i++){
-    if(/\{\d+:\s+\}\n?/i.test(unformattedNotes[i][0])){
-      unformattedNotes[i][2] = ""
+    for(ii = 0; ii < state.notesRecord.length; ii++){
+      if(state.notesRecord[ii].record == unformattedNotes[i][0]){
+        state.notesRecordMatch = true
+      }
     }
-    tempNote = unformattedNotes[i][0].replace(`{${unformattedNotes[i][1]}:`, "")
+    if(/\{\d*[\s-,\.]?\d+:\s+\}\n?/i.test(unformattedNotes[i][0])){
+      unformattedNotes[i][4] = ""
+    }
+    tempNote = unformattedNotes[i][0].replace(`{${unformattedNotes[i][1]}${unformattedNotes[i][2]}:`, "")
     tempNote = tempNote.replace("}\n", "}")
     tempNote = tempNote.replace("}", "")
-    tempLine = Number(unformattedNotes[i][1])
-    tempObject = {note: tempNote, line: tempLine}
-    if(unformattedNotes[i][2]){
+    tempLine = Number(unformattedNotes[i][3])
+    tempEndBy = Number(unformattedNotes[i][1]) ? info.actionCount + Number(unformattedNotes[i][1]) : 0
+    tempRecord = {record: unformattedNotes[i][0], endBy: tempEndBy}
+    tempObject = {record: unformattedNotes[i][0], note: tempNote, line: tempLine}
+    if(unformattedNotes[i][4]){
       state.notes.push(tempObject)
     }
+    if(!state.notesRecordMatch){
+      state.notesRecord.push(tempRecord)
+    }
+    state.notesRecordMatch = false
   }
   for(i = 0; i < editorsNotes.length; i++){
-    if(/\{e\/?n\d+:\s+\}\n?/i.test(editorsNotes[i][0])){
-      editorsNotes[i][3] = ""
+    for(ii = 0; ii < state.notesRecord.length; ii++){
+      if(state.notesRecord[ii].record == editorsNotes[i][0]){
+        state.notesRecordMatch = true
+      }
     }
-    tempNote = editorsNotes[i][0].replace(`{${editorsNotes[i][1]}`, "[Editor's note")
+    if(/\{\d*e\/?n\d+:\s+\}\n?/i.test(editorsNotes[i][0])){
+      editorsNotes[i][4] = ""
+    }
+    tempNote = editorsNotes[i][0].replace(`{${editorsNotes[i][1]}${editorsNotes[i][2]}`, "[Editor's note")
     tempNote = tempNote.replace("}\n", "}")
     tempNote = tempNote.replace("}", "]")
-    tempLine = Number(editorsNotes[i][2])
-    tempObject = {note: tempNote, line: tempLine}
-    if(editorsNotes[i][3]){
+    tempLine = Number(editorsNotes[i][3])
+    tempEndBy = Number(editorsNotes[i][1]) ? info.actionCount + Number(editorsNotes[i][1]) : 0
+    tempRecord = {record: editorsNotes[i][0], endBy: tempEndBy}
+    tempObject = {record: editorsNotes[i][0], note: tempNote, line: tempLine}
+    if(editorsNotes[i][4]){
       state.notes.push(tempObject)
     }
+    if(!state.notesRecordMatch){
+      state.notesRecord.push(tempRecord)
+    }
+    state.notesRecordMatch = false
   }
   for(i = 0; i < editorsNotesScene.length; i++){
-    if(/\{e\/?ns\d+:\s+\}\n?/i.test(editorsNotesScene[i][0])){
-      editorsNotesScene[i][3] = ""
+    for(ii = 0; ii < state.notesRecord.length; ii++){
+      if(state.notesRecord[ii].record == editorsNotesScene[i][0]){
+        state.notesRecordMatch = true
+      }
     }
-    tempNote = editorsNotesScene[i][0].replace(`{${editorsNotesScene[i][1]}:`, "[Editor's note: this scene:<")
+    if(/\{\d*e\/?ns\d+:\s+\}\n?/i.test(editorsNotesScene[i][0])){
+      editorsNotesScene[i][4] = ""
+    }
+    tempNote = editorsNotesScene[i][0].replace(`{${editorsNotesScene[i][1]}${editorsNotesScene[i][2]}:`, "[Editor's note: this scene:<")
     tempNote = tempNote.replace("}\n", "}")
     tempNote = tempNote.replace("}", ">.]")
-    tempLine = Number(editorsNotesScene[i][2])
-    tempObject = {note: tempNote, line: tempLine}
-    if(editorsNotesScene[i][3]){
+    tempLine = Number(editorsNotesScene[i][3])
+    tempEndBy = Number(editorsNotesScene[i][1]) ? info.actionCount + Number(editorsNotesScene[i][1]) : 0
+    tempRecord = {record: editorsNotesScene[i][0], endBy: tempEndBy}
+    tempObject = {record: editorsNotesScene[i][0], note: tempNote, line: tempLine}
+    if(editorsNotesScene[i][4]){
       state.notes.push(tempObject)
     }
+    if(!state.notesRecordMatch){
+      state.notesRecord.push(tempRecord)
+    }
+    state.notesRecordMatch = false
   }
   //this removes any empty author's notes
   //which can generate if the A/N field is used for Note++ notes
@@ -86,6 +136,20 @@ function noteTextParser (){
 //This injects the reformatted Note++ objects into the context
 function noteTextInjector (){
   for (i = 0; i < state.notes.length; i++){
+    for (ii = 0; ii < state.notesRecord.length; ii++){
+      if(state.notes[i].record == state.notesRecord[ii].record){
+        state.notesEndBy = state.notesRecord[ii].endBy
+      }
+    }
+    if(state.notesEndBy && info.actionCount < state.notesEndBy){
+      if(state.notes[i].line > 0){
+        if(lines.length > (state.notes[i].line - 1)){
+          lines.splice(-(state.notes[i].line), 0, state.notes[i].note)
+        } 
+      } else if (state.notes[i].line == 0) {
+        lines.push(state.notes[i].note)
+      }
+    } else if (!state.notesEndBy){
       if(state.notes[i].line > 0){
         if(lines.length > (state.notes[i].line - 1)){
           lines.splice(-(state.notes[i].line), 0, state.notes[i].note)
@@ -94,4 +158,6 @@ function noteTextInjector (){
         lines.push(state.notes[i].note)
       }
     }
+    state.notesEndBy = 0
   }
+}
