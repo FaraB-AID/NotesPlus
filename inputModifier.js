5d052e5188f2b8658509f387233a3c076904d2a0
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-03-21 00:00:19 -0400

    Update inputModifier.js

--- inputModifier.js
+++ inputModifier.js
@@ -8,6 +8,14 @@ const modifier = (text) => {
     state.lmiToggle = true
   }
 
+  //at a request, I included a command to toggle the AI
+  state.message = ""
+  if(/\n? ?(> You |> You say "|)(\/ai)/i.test(text)){
+    state.noteAIOff = state.noteAIOff ? false : true
+    state.message = state.noteAIOff ? "The AI has been turned off." : "The AI has been turned on."
+    text = null; stop = true; return{ text, stop }
+  }
+
   return { text: modifiedText }
 }
 
