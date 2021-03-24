27c596d7b9c4876f37529a48650803994b23fc7d
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-03-24 14:04:14 -0400

    Update contextModifier.js

--- contextModifier.js
+++ contextModifier.js
@@ -6,20 +6,17 @@ const lines = text.split("\n")
 const modifier = (text) => {
   //this function injects reformatted Note++ notes into context
   noteTextInjector ()
-  
-  //this records the last model input for the /lmi command
-  state.lmi = lines.join("\n").slice(-(info.maxChars))
-  
-  //this prevents submission to the AI if the /lmi command is used
-  if(state.lmiToggle){
-    state.lmiToggle = false
-    stop = true; return{ stop }
-  }
-  
-  //this prevents the AI from running if the AI is toggled off
-  if(state.noteAIOff){
-    stop = true; return{ stop }
-  }
+
+  //this prevents submission to the AI if a command is used
+if(state.contextStop){
+  state.contextStop = false
+  stop = true; return{ stop }
+}
+
+//this prevents the AI from running if /ai is toggled off
+if(state.noteAIOff){
+  stop = true; return{ stop }
+}
 
   const finalText = lines.join("\n")
   return { text: finalText }
