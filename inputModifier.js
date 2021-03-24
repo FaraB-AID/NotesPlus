7c25a3a513d9baa90addab62dfa0cc88e860241c
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-03-24 14:03:37 -0400

    Update inputModifier.js

--- inputModifier.js
+++ inputModifier.js
@@ -1,20 +1,12 @@
+//implements Notes++ commands
+notesCommands ()
+
+//modifies input with Notes++ special input formats
+noteInputSpecial()
+
 const modifier = (text) => {
   let modifiedText = text
-  const lowered = text.toLowerCase()
-
-  //this implements a command to display the last model input
-  if(/\n? ?(> You |> You say "|)(\/lmi)/i.test(text)) {
-    modifiedText = "\n" + state.lmi
-    state.lmiToggle = true
-  }
-
-  //at a request, I included a command to toggle the AI
-  state.message = ""
-  if(/\n? ?(> You |> You say "|)(\/ai)/i.test(text)){
-    state.noteAIOff = state.noteAIOff ? false : true
-    state.message = state.noteAIOff ? "The AI has been turned off." : "The AI has been turned on."
-    text = null; stop = true; return{ text, stop }
-  }
+  const lowered = text?.toLowerCase()
 
   return { text: modifiedText }
 }
