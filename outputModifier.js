0e9bd7f0cbeffe75cdab6ff213218cc788c336f8
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-03-24 14:04:42 -0400

    Create outputModifier.js

---
+++ outputModifier.js
@@ -0,0 +1,11 @@
+//modifies output with Notes++ special output formats
+noteOutputSpecial()
+
+const modifier = (text) => {
+  let modifiedText = text
+  const lowered = text?.toLowerCase()
+    
+  return { text: modifiedText }
+}
+
+modifier(text)
