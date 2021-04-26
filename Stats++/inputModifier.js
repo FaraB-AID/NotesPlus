b3817ba4e18b57ffc09ff3069e5545ff7b3ee8b7
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-04-26 14:31:47 -0400

    Create inputModifier.js

---
+++ Stats++/inputModifier.js
@@ -0,0 +1,14 @@
+//implements Notes++ commands
+notesCommands()
+
+//modifies input with Notes++ special input formats
+noteInputSpecial()
+
+const modifier = (text) => {
+  let modifiedText = text
+  const lowered = text?.toLowerCase()
+
+  return { text: modifiedText }
+}
+
+modifier(text)
