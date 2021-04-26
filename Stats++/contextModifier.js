6b32afaa334639be025e6a404ef4215238524d03
FaraB <78620899+FaraB-AID@users.noreply.github.com>
2021-04-26 14:32:11 -0400

    Create contextModifier.js

---
+++ Stats++/contextModifier.js
@@ -0,0 +1,29 @@
+//this function tracks output count
+noteTimeKeeper()
+//this function finds, parses, and extracts Note++ notes
+noteTextParser()
+//these functions inject message and display special format notes
+noteMessageInjector()
+noteDisplayInjector()
+
+const lines = text.split("\n")
+
+const modifier = (text) => {
+  //this function injects reformatted Note++ notes into context
+  noteTextInjector()
+
+  //this prevents submission to the AI if a command is used
+if(state.contextStop){
+  state.contextStop = false
+  stop = true; return{ stop }
+}
+
+//this prevents the AI from running if /ai is toggled off
+if(state.noteAIOff){
+  stop = true; return{ stop }
+}
+
+  const finalText = lines.join("\n")
+  return { text: finalText }
+} 
+modifier(text)
